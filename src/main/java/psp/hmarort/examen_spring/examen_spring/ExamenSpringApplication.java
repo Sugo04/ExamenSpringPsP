5d8f46e85a659f0f444247903d41be1a13ef207b
Héctor <hmarort075@g.educaand.es>
2024-11-26 10:40:48 +0100

    Creación Carpeta de examen

---
+++ src/main/java/psp/hmarort/examen_spring/examen_spring/ExamenSpringApplication.java
@@ -0,0 +1,13 @@
+package psp.hmarort.examen_spring.examen_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psp/hmarort/examen_spring/examen_spring/ExamenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package psp.hmarort.examen_spring.examen_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
