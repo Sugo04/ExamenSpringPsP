0204173990b7bfa845c2142a6fbf751915905c68
Héctor <hmarort075@g.educaand.es>
2024-11-26 19:59:54 +0100

    Cagada Monumental solucionada no más @Controller sino @RestController

--- src/main/java/psp/hmarort/examen_spring/examen_spring/controllers/Controller2.java
+++ src/main/java/psp/hmarort/examen_spring/examen_spring/controllers/Controller2.java
@@ -1,11 +1,11 @@
 package psp.hmarort.examen_spring.examen_spring.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /* Héctor Martín Ortega 2º FPGS Desarrollo de Aplicaciones Multiplataforma */
 /* DNI: 45336075Q */
-@Controller
+@RestController
 @RequestMapping("/ejercicio2")
 public class Controller2 {
 
